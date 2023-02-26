733c5e66109457039987615d0f5564cf9db02fcb
Vardges Avetisyan <vavetisyan.g@gmail.com>
2023-02-26 14:11:21 +0400

    initialized firebase app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDxf1CTXs8Emb08__tZCAiQu9GM6CQqZDU",
+    authDomain: "translate-df955.firebaseapp.com",
+    projectId: "translate-df955",
+    storageBucket: "translate-df955.appspot.com",
+    messagingSenderId: "1044555473445",
+    appId: "1:1044555473445:web:68ab8fedb2f66de75f95cc",
+    measurementId: "G-FYZ9QKRXCC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const App = () => {
+    console.log(app)
     return (
         <div>
           app
