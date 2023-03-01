5479e5a2b649612ac369b5a6f848ee070dc0041e
Vardges Avetisyan <vavetisyan.g@gmail.com>
2023-03-01 11:56:20 +0400

    fix translate error

--- src/views/LanguageDetails/EditableCell/index.js
+++ src/views/LanguageDetails/EditableCell/index.js
@@ -1,50 +1,52 @@
-import {Form, Input} from "antd";
+import {Col, Form, Input, Row} from "antd";
+import {memo, useRef} from "react";
+import {TranslationOutlined} from "@ant-design/icons";
 
-const EditableCell = ({
-                          editing,
-                          dataIndex,
-                          title,
-                          inputType,
-                          record,
-                          index,
-                          children,
-                          data,
-                          setData,
-                          handleTranslate,
-                          ...restProps
-                      }) => {
+const EditableCell = memo(({
+                               editing,
+                               dataIndex,
+                               title,
+                               inputType,
+                               record,
+                               index,
+                               children,
+                               data,
+                               setData,
+                               handleTranslate,
+                               form,
+                               ...restProps
+                           }) => {
     return (
         <td {...restProps}>
             {editing ? (
-                <Form.Item
-                    name={dataIndex}
-                    style={{margin: 0}}
-                    rules={[{required: true, message: `Please Input ${title}!`}]}
-                >
-                    <Input
-                        onBlur={(e) => {
-                            setData(
-                                data.map((item) =>
-                                    item.id === record.id
-                                        ? {...item, [dataIndex]: e.nativeEvent.target.value}
-                                        : item
-                                )
+                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    <Form.Item
+                        name={dataIndex}
+                        style={{margin: 0}}
+                        rules={[{required: true, message: `Please Input ${dataIndex}!`}]}>
+                        <Input/>
+                    </Form.Item>
+                    <div style={{padding: 10, cursor: 'pointer'}} onClick={(e) => {
+                        setData(
+                            data.map((item) =>
+                                item.id === record.id
+                                    ? {...item, [dataIndex]: form.getFieldsValue()[dataIndex]}
+                                    : item
                             )
-                            handleTranslate({
-                                text: e.nativeEvent.target.value,
-                                language: dataIndex,
-                            })
-                        }}
-                    />
-                </Form.Item>
+                        )
+                        handleTranslate({
+                            text: form.getFieldsValue()[dataIndex],
+                            language: dataIndex,
+                        })
+                    }}>
+                        <TranslationOutlined style={{fontSize: '20px', color: '#1E3045'}}/>
+                    </div>
+                </div>
             ) : (
                 children
             )}
         </td>
     );
-
-
-
-};
+});
 
 export default EditableCell;
